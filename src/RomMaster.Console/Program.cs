bb8685e4b80d941ea6263e4b3477eaba0b1f7a64
wally <wally666@gmail.com>
2018-06-20 20:37:19 +0200

    added unit test

--- src/RomMaster.Console/Program.cs
+++ src/RomMaster.Console/Program.cs
@@ -41,45 +41,5 @@
 
             await builder.RunConsoleAsync();
         }
-
-        //const string datFileRootFolder = @"z:\RomVault\DatRoot";
-
-        //static void Main(string[] args)
-        //{
-        //    RomMaster.FileSystemWatcher.Watcher w = new RomMaster.FileSystemWatcher.Watcher(@"z:\RomVault\ToSort");
-        //    w.Enabled = true;
-
-        //    RomMaster.FileSystemWatcher.Watcher watcherDatRoot = new RomMaster.FileSystemWatcher.Watcher(datFileRootFolder);
-        //    watcherDatRoot.Enabled = true;
-
-        //    Console.ReadLine();
-
-        //    var outputFilePathName = @"c:\repo\github_wally666\rommaster\src\RomMaster.Tests\JDownloader.dat";
-        //    var filePathName = @"c:\repo\github_wally666\rommaster\src\RomMaster.Tests\fixDat_Sony - PlayStation Portable (20180309-050057).dat";
-        //    var parser = new RomMaster.DatFileParser.Parser();
-        //    var model = parser.Parse(filePathName);
-
-        //    filePathName = @"c:\repo\github_wally666\rommaster\src\RomMaster.Tests\1fichier.com-PSP.txt";
-        //    using (var sr = new StreamReader(filePathName))
-        //    using (var sw = new StreamWriter(outputFilePathName))
-        //    {
-        //        var regex = new System.Text.RegularExpressions.Regex("^(?<GAME_NAME>.*?)\\.7z");
-        //        while (!sr.EndOfStream)
-        //        {
-        //            var line = sr.ReadLine();
-        //            var gameName = regex.Match(line).Groups["GAME_NAME"].Value;
-
-        //            var game = model.Games.FirstOrDefault(a => a.Name == gameName);
-        //            if (game != null)
-        //            {
-        //                System.Console.WriteLine(line);
-        //                sw.WriteLine(line);
-        //            }
-        //        }
-
-        //        sw.Flush();
-        //        sw.Close();
-        //    }
-        //}
     }
 }
--- src/RomMaster.Tests/DatFileParser/DatFileParserTests.cs
+++ src/RomMaster.Tests/DatFileParser/DatFileParserTests.cs
@@ -2,6 +2,7 @@
 {
     using FluentAssertions;
     using System.IO;
+    using System.Linq;
     using Xunit;
 
     public class DatFileParserTests
@@ -37,6 +38,47 @@
                 var result = parser.Parse(stream);
 
                 result.Should().NotBeNull();
+            }
+        }
+
+        [Theory]
+        [InlineData("fixDat_Sony - PlayStation Portable (20180309-050057).dat")]
+        public void Validate_ForValidDatStream_DoesNotThrowException(string filePathName)
+        {
+            using (var stream = new FileStream(filePathName, FileMode.Open))
+            {
+                parser.Validate(stream);
+            }
+        }
+
+        [Theory]
+        [InlineData("fixDat_Sony - PlayStation Portable (20180309-050057).dat")]
+        public void FilterGeneratedReport(string filePathName)
+        { 
+            var outputFilePathName = @"JDownloader.dat";
+            var parser = new RomMaster.DatFileParser.Parser();
+            var model = parser.Parse(filePathName);
+
+            filePathName = @"1fichier.com-PSP.txt";
+            using (var sr = new StreamReader(filePathName))
+            using (var sw = new StreamWriter(outputFilePathName))
+            {
+                var regex = new System.Text.RegularExpressions.Regex("^(?<GAME_NAME>.*?)\\.7z");
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine();
+                    var gameName = regex.Match(line).Groups["GAME_NAME"].Value;
+
+                    var game = model.Games.FirstOrDefault(a => a.Name == gameName);
+                    if (game != null)
+                    {
+                        System.Console.WriteLine(line);
+                        sw.WriteLine(line);
+                    }
+                }
+
+                sw.Flush();
+                sw.Close();
             }
         }
     }
