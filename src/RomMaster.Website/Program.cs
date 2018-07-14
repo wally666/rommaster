3ce4d15f4229c0600a790f13741b4668d6d0e29f
wally <wally666@gmail.com>
2018-07-14 02:10:54 +0200

    Update .Net Core => 2.1.1

--- src/RomMaster.Website/Program.cs
+++ src/RomMaster.Website/Program.cs
@@ -19,6 +19,7 @@ namespace RomMaster.Website
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseDefaultServiceProvider(options => options.ValidateScopes = false)
                 .UseStartup<Startup>();
     }
 }
--- src/RomMaster.Website/Startup.cs
+++ src/RomMaster.Website/Startup.cs
@@ -3,8 +3,18 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using RomMaster.BusinessLogic.Services;
+using RomMaster.Client.Database;
+using RomMaster.Common;
+using RomMaster.Common.Database;
+using RomMaster.DatFileParser;
+using System.Security.Cryptography;
 
 namespace RomMaster.Website
 {
@@ -27,10 +37,30 @@ namespace RomMaster.Website
             {
                 configuration.RootPath = "ClientApp/dist";
             });
+
+            // console
+            services.AddOptions()
+                .Configure<AppSettings>(Configuration.GetSection("AppSettings"))
+                .AddDbContext<DatabaseContext>(options =>
+                {
+                    options.UseSqlite(Configuration.GetSection("AppSettings")
+                            .GetConnectionString("sqlite"))
+                        .EnableSensitiveDataLogging(false);
+                }, ServiceLifetime.Scoped)
+                .AddTransient<IUnitOfWorkFactory, UnitOfWorkFactory>()
+                .AddSingleton<Parser>()
+                .AddSingleton<FileWatcherService>()
+                .AddSingleton<DatFileService>()
+                .AddSingleton<RomFileService>()
+                .AddSingleton<ToSortFileService>()
+                .AddSingleton<FixService>()
+                .AddSingleton<HashAlgorithm, Force.Crc32.Crc32Algorithm>()
+                .AddSingleton<IHostedService, ClientService>()
+                .Replace(ServiceDescriptor.Singleton(typeof(ILogger<>), typeof(TimedLogger<>)));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, Microsoft.AspNetCore.Hosting.IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
