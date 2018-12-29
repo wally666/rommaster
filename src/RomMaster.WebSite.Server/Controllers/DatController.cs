4bc1b1ea94dbeb93fbb8c49480f19de315747f64
wally <wally666@gmail.com>
2018-12-29 01:59:00 +0100

    WebSite project (WebAssembly)

---
+++ src/RomMaster.WebSite.Server/Controllers/DatController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RomMaster.Client.Database.Models;
+using RomMaster.Common.Database;
+
+namespace RomMaster.WebSite.App.Controllers
+{
+    [Route("api/[controller]")]
+    public class DatController : Controller
+    {
+        private readonly ILogger<DatController> logger;
+        private readonly IUnitOfWorkFactory unitOfWorkFactory;
+
+        public DatController(ILogger<DatController> logger, IUnitOfWorkFactory unitOfWorkFactory)
+        {
+            this.logger = logger;
+            this.unitOfWorkFactory = unitOfWorkFactory;
+        }
+
+        public IEnumerable<Dat> GetDats()
+        {
+            using (var uow = unitOfWorkFactory.Create())
+            {
+                var repo = uow.GetRepository<Dat>();
+                return repo.GetAll();
+            }
+        }
+    }
+}
--- src/RomMaster.WebSite.Server/Startup.cs
+++ src/RomMaster.WebSite.Server/Startup.cs
@@ -2,20 +2,44 @@ using Microsoft.AspNetCore.Blazor.Server;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+//using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Serialization;
+using RomMaster.BusinessLogic.Services;
+using RomMaster.Client.Database;
+using RomMaster.Common;
+using RomMaster.Common.Database;
+using RomMaster.DatFileParser;
 using System.Linq;
 using System.Net.Mime;
+using System.Security.Cryptography;
 
 namespace RomMaster.WebSite.Server
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             // Adds the Server-Side Blazor services, and those registered by the app project's startup.
             services.AddServerSideBlazor<App.Startup>();
+
+            services.AddMvc().AddJsonOptions(options =>
+            {
+                options.SerializerSettings.ContractResolver = new DefaultContractResolver();
+            });
 
             services.AddResponseCompression(options =>
             {
@@ -25,6 +49,28 @@ namespace RomMaster.WebSite.Server
                     WasmMediaTypeNames.Application.Wasm,
                 });
             });
+
+            // console
+            services.AddOptions()
+                .Configure<AppSettings>(Configuration.GetSection("AppSettings"))
+                .AddDbContext<DatabaseContext>(options =>
+                {
+                    options.UseSqlite(Configuration.GetSection("AppSettings")
+                            .GetConnectionString("sqlite"))
+                        .EnableSensitiveDataLogging(false);
+                }, ServiceLifetime.Singleton)
+                .AddTransient<IUnitOfWorkFactory, UnitOfWorkFactory>()
+
+                .AddSingleton<Parser>()
+                .AddSingleton<FileWatcherService>()
+                .AddSingleton<DatFileService>()
+                .AddSingleton<RomFileService>()
+                .AddSingleton<ToSortFileService>()
+                .AddSingleton<FixService>()
+                .AddSingleton<HashAlgorithm, Force.Crc32.Crc32Algorithm>()
+
+                .AddSingleton<Microsoft.Extensions.Hosting.IHostedService, ClientService>()
+                .Replace(ServiceDescriptor.Singleton(typeof(ILogger<>), typeof(TimedLogger<>)));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,6 +82,11 @@ namespace RomMaster.WebSite.Server
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(name: "default", template: "{controller}/{action}/{id?}");
+            });
 
             // Use component registrations and static files from the app project.
             app.UseServerSideBlazor<App.Startup>();
